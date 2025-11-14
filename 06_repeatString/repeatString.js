4cd4a19b4dc9636ff1b97c9914fb486f5df789be
Santiago <santiago800g@gmail.com>
2025-11-14 13:25:25 -0600

    Solve exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    repeatedString = '';
+    if (times < 0) {
+        return 'ERROR';
+    }
+    for(let i = 0 ; i < times ; i++){
+        repeatedString += str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
