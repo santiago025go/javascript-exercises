95f352c0d0bf039b9cb4b34a2b33de7263a6e33c
Santiago <santiago800g@gmail.com>
2025-11-14 15:25:39 -0600

    Solve exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(majorNumber, minorNumber) {
+   if(!Number.isInteger(majorNumber) || !(majorNumber >= 0) || !Number.isInteger(minorNumber) || !(minorNumber >= 0)){
+        return 'ERROR';
+   }
+   let sum = 0, temp;
+   if(minorNumber > majorNumber){
+    temp = majorNumber;
+    majorNumber = minorNumber;
+    minorNumber = temp;
+   }
+   for(let i = minorNumber ; i <= majorNumber ; i++){
+    sum += i;
+   }
+   return sum;
 };
 
 // Do not edit below this line
