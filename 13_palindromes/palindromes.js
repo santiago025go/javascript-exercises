c8a1547d110f4ca67a51bfcfdfb4559c01e9d7db
Santiago <santiago800g@gmail.com>
2025-11-26 17:15:15 -0600

    Solve exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let permitedCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    str = str.toLowerCase().split('').filter((item) => permitedCharacters.includes(item)).join('');
+    let strReversed = str.split('').reverse().join('');
+    return str === strReversed;
 };
 
 // Do not edit below this line
