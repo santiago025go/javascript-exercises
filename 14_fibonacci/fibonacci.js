bc21ec5c3874ea2a21d5631b39de4763ee04a702
Santiago <santiago800g@gmail.com>
2025-11-26 17:29:30 -0600

    Solve exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = Number(position);
+    if(position === NaN || position < 0) return 'OOPS';
+    let secuence = [0, 1];
+    for(let i = 2; i <= position ; i++){
+        secuence[i] = secuence[i - 2] + secuence[i -1];
+    }
+    return secuence[position];
 };
 
 // Do not edit below this line
