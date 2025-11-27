74d3f278e2bead57c04c1176b25018942abfe250
Santiago <santiago800g@gmail.com>
2025-11-26 20:00:45 -0600

    Solve exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => (('yearOfDeath' in b ? b.yearOfDeath : 2025) - b.yearOfBirth) - (('yearOfDeath' in a ? a.yearOfDeath : 2025) - a.yearOfBirth));
+    return people[0];
 };
 
 // Do not edit below this line
